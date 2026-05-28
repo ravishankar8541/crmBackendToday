56ab8ecb32d052b813e8e9dacd5219b449f5d1ae
unknown <ravishankarray480@gmail.com>
2026-05-28 16:26:55 +0530

    updated fronend url

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 dbConnection();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://cr-mfrontend-liart.vercel.app', 
   credentials: true,               
 }));
 app.use(express.json());
